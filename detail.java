490db8d14993616d3e7fb364a5f01d6b745cf39e
Mohammed Abdul Khader <abdulkhader0404@gmail.com>
2025-07-25 12:08:41 +0530

    Create detail.java

---
+++ detail.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class detail
+{
+	public static void main(String[] args)
+	{
+		String name = "Khader";
+		String rno = "24512250XXX";
+		String dept = "CSE";
+		System.out.println("Name : "+name);
+		System.out.println("Roll no : "+rno);
+		System.out.println("Department : "+dept);
+	}
+}
